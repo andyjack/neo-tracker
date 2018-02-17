ddb17a4f49ff2bcbf2adca30f74f24e43c761037
Andy Jack <github@veracity.ca>
2018-02-17 00:51:11 -0500

    Add a default error handler

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ process.env.PATH = `${process.env.PATH}:./node_modules/.bin`;
 app.use('/app/current', require('./routes/current'));
 app.use('/app/sparkline', require('./routes/sparkline'));
 app.use('/app/update', require('./routes/update'));
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('kablooie!')
+})
 
 async function main() {
   db.startup()
