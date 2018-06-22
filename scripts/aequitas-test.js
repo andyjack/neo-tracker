77c85fead753b6dd6884b6b88700d6f313b509f0
Andy Jack <github@veracity.ca>
2018-06-22 15:17:02 -0400

    Aequitas price testing script

---
+++ scripts/aequitas-test.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+const getPrices = require('../lib/getPrices.js');
+
+const rows = [{ symbol: 'ICO' }, { symbol: 'HIP' }, { symbol: 'HUGE' }];
+// eslint-disable-next-line no-console
+getPrices.aequitas(rows).then(prices => console.dir(prices));
