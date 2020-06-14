e6cdf0126bb63d3d881115beef827452ce456a04
Andy Jack <github@veracity.ca>
2020-06-14 19:36:49 -0400

    Fix sqlite api usage, try three

--- scripts/db-test.js
+++ scripts/db-test.js
@@ -3,7 +3,21 @@
 const db = require('../lib/db');
 
 (async function () {
-  db.startup()
+  await db
+    .startup()
     // eslint-disable-next-line no-console
     .catch((err) => console.error(err.stack));
+
+  db.sqlite()
+    .all(
+      'SELECT date,price FROM daily_price JOIN stock USING (stock_id) ' +
+        ' WHERE symbol = ? AND date > date(?,?) AND strftime(?,date) = ?' +
+        ' ORDER BY date DESC',
+      'TSX:CBO',
+      'now',
+      '-1 year',
+      '%w',
+      '1' // only want Mondays
+    )
+    .then((rows) => console.dir(rows));
 })();
